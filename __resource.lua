817e394b5237939121238998871b737c7dd204f9
golex <golex@twilight-eclipse.com>
2017-06-22 23:22:19 -0500

    Rename from test -> contextMenu

--- __resource.lua
+++ __resource.lua
@@ -2,8 +2,8 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 ui_page 'html/contextMenu.html'
 
-client_script 'golextest_client.lua'
-server_script 'golextest_server.lua'
+client_script 'contextMenu_client.lua'
+server_script 'contextMenu_server.lua'
 
 files {
     'html/contextMenu.html',
